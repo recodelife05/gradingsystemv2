bc74f3fd0762bfc0b098d95cc459b8c0fd1716fa
Mark Anthony Laqui <mark.laqui@randemgroup.com>
2024-02-03 13:19:19 +0800

    added table for grading

--- src/GradingPage.java
+++ src/GradingPage.java
@@ -1,18 +1,41 @@
+import javax.sound.midi.SysexMessage;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class GradingPage extends JFrame {
 
-    JLabel totalActivity = new JLabel();
+    JLabel totalActivityLabel = new JLabel();
+    JLabel totalLabLabel = new JLabel();
+    JLabel totalExamLabel = new JLabel();
+    JLabel finalGradeLabel = new JLabel();
     JTextField activityTextField = new JTextField();
-    DefaultListModel<Integer> activityGrades = new DefaultListModel<>();
+    DefaultListModel<Double> activityGrades = new DefaultListModel<>();
+    DefaultListModel<Double> labGrades = new DefaultListModel<>();
+    DefaultListModel<Double> examGrades = new DefaultListModel<>();
+    JButton activityComputeBtn = new JButton("Add Compute Activity");
+    JButton examComputeBtn = new JButton("Add Exam Score");
+    JButton labComputeBtn = new JButton("Add Lab Score");
+    JButton finalGradeBtn = new JButton("Compute Final Grade");
+    JTable activityTable = new JTable();
+    DefaultTableModel model = new DefaultTableModel();
+    String ACTIVITYGRADE = "Activity Grade";
+    String LABGRADE = "Laboratory Grade";
+    String EXAMGRADE = "Exam Grade";
+    String FINALEGRADE = "Final Grade";
+    String[][] data = { };
+    double TotalActivity = 0;
+    double TotalExam = 0;
+    double TotalLab = 0;
 
-    JButton activityComputeBtn = new JButton("Compute Activity");
-    Integer TotalActivity = 0;
     GradingPage(){
         InitializedComponents();
         SetEvents();
@@ -22,101 +45,111 @@ public class GradingPage extends JFrame {
         this.setTitle("Grading System");
         this.setSize(500,500);
         this.setLayout(new GridLayout(1, 3, 10, 10));
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setBounds(new Rectangle(500,500));
-        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBounds(new Rectangle(500,500));
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
-        activityGrades.add(0,60);
-        activityGrades.add(1,90);
-        JList<Integer> ac = new JList<>(activityGrades);
+        String[] columnNames = { "Grade Type", "Score"};
 
-        MyCellRenderer renderer = new MyCellRenderer();
-        ac.setCellRenderer(renderer);
-        ac.setToolTipText("Activity Grades");
-        ac.setVisible(true);
+        model = (DefaultTableModel) activityTable.getModel();
+        activityTable = new JTable(model);
+        activityTable.setBounds(30, 40, 200, 300);
 
-        ac.setSize(200,200);
-        bottomPanel.add(ac);
-        bottomPanel.add(activityComputeBtn);
-        bottomPanel.add(totalActivity);
-        bottomPanel.add(activityTextField);
+        Arrays.stream(columnNames).forEach(f -> {
+            model.addColumn(f);
+        });
 
+        mainPanel.add(activityComputeBtn);
+        mainPanel.add(labComputeBtn);
+        mainPanel.add(examComputeBtn);
+        mainPanel.add(finalGradeBtn);
+        mainPanel.add(totalActivityLabel);
+        mainPanel.add(totalLabLabel);
+        mainPanel.add(totalExamLabel);
+        mainPanel.add(finalGradeLabel);
+        mainPanel.add(activityTextField);
+
+        mainPanel.add(new JScrollPane(activityTable));
 
         ((JComponent) getContentPane()).setBorder(new EmptyBorder(15, 15, 15, 15));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+        getContentPane().add(mainPanel, BorderLayout.SOUTH);
         pack();
         setLocationRelativeTo(null);
-    }
-    public class MyCellRenderer extends JLabel implements ListCellRenderer<Object> {
-        public MyCellRenderer() {
-            setOpaque(true);
-        }
-
-        public Component getListCellRendererComponent(JList<?> list,
-                                                      Object value,
-                                                      int index,
-                                                      boolean isSelected,
-                                                      boolean cellHasFocus) {
-
-            setText(value.toString());
-
-            Color background;
-            Color foreground;
-
-            // check if this cell represents the current DnD drop location
-            JList.DropLocation dropLocation = list.getDropLocation();
-            if (dropLocation != null
-                    && !dropLocation.isInsert()
-                    && dropLocation.getIndex() == index) {
-
-                background = Color.BLUE;
-                foreground = Color.WHITE;
-
-                // check if this cell is selected
-            } else if (isSelected) {
-                background = Color.GREEN;
-                foreground = Color.WHITE;
-
-                // unselected, and not the DnD drop location
-            } else {
-                background = Color.WHITE;
-                foreground = Color.BLACK;
-            };
-
-            setBackground(background);
-            setForeground(foreground);
-
-            return this;
-        }
     }
     private void SetEvents(){
         activityComputeBtn.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
                 TotalActivity = 0;
-                Integer currentScore = Integer.parseInt(activityTextField.getText());
+                Double currentScore = Double.parseDouble(activityTextField.getText());
+                model.addRow(new Object[] {ACTIVITYGRADE,currentScore.toString()});
                 activityGrades.add(0,currentScore);
-
                 activityGrades.elements().asIterator()
                         .forEachRemaining(r ->
-                                        TotalActivity +=  r.intValue()
+                                        TotalActivity += r.doubleValue()
                                 );
+                System.out.println("activity grades count" + activityGrades.getSize());
+                totalActivityLabel.setText("Total " + ACTIVITYGRADE + ":" + TotalActivity / activityGrades.getSize());
+            }
+        });
 
-                totalActivity.setText(TotalActivity.toString());
+        labComputeBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                TotalLab = 0;
+                Double currentScore = Double.parseDouble(activityTextField.getText());
+                model.addRow(new Object[] {LABGRADE,currentScore.toString()});
+                labGrades.add(0,currentScore);
+                labGrades.elements().asIterator()
+                        .forEachRemaining(r ->
+                                TotalLab +=  r.doubleValue()
+                        );
+
+                totalLabLabel.setText(String.format("Total " + LABGRADE + ": %.2f", TotalLab / labGrades.getSize()));
+
+            }
+        });
+
+        examComputeBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                TotalExam = 0;
+                Double currentScore = Double.parseDouble(activityTextField.getText());
+                model.addRow(new Object[] {EXAMGRADE,currentScore.toString()});
+                examGrades.add(0,currentScore);
+                examGrades.elements().asIterator()
+                        .forEachRemaining(r ->
+                                TotalExam +=  r.doubleValue()
+                        );
+
+                totalExamLabel.setText(String.format("Total " + EXAMGRADE + ": %.2f", TotalExam / examGrades.getSize()));
+            }
+        });
+
+        finalGradeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ComputeFinalGrade();
             }
         });
     }
 
-    public void ComputeActivity(){
+    public void ComputeFinalGrade(){
 
+        //exam * 40, lab 30percent , activity .30
+        System.out.println("total lab: " + (TotalLab/ labGrades.getSize()) * 0.30);
+        System.out.println("total exam: " + ((TotalExam / examGrades.getSize()) * 0.40));
+        System.out.println("total activity: " +((TotalActivity / activityGrades.getSize()) * 0.30));
+        double finalGrade = ((TotalLab/ labGrades.getSize()) * 0.30) +
+                ((TotalActivity / activityGrades.getSize()) * 0.30) +
+                ((TotalExam / examGrades.getSize()) * 0.40);
+        finalGradeLabel.setText(String.format(FINALEGRADE + ": %.2f", finalGrade));
+        if (finalGrade < 70) {
+            finalGradeLabel.setForeground(Color.RED);
+        } else {
+            finalGradeLabel.setForeground(Color.BLACK);
+        }
     }
-    public class ActivityGrade {
 
-        public int Score;
-    }
-
-    public void AddActivityScore(){
-
-    }
 }
